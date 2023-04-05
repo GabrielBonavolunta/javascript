0056a9dc7b34165fb3f7eeee7cea44e947b0ef96
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-04-05 17:44:03 -0300

    Commit aula16

---
+++ aula16/index.js
@@ -0,0 +1,34 @@
+//            012345678910
+const nome = "Luiz Otávio";
+
+//                 0       1        2
+const alunos = ["Luiz", "Maria", "João"];
+
+alunos.push("Luiza");
+alunos.push("Fábio");
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//console.log(alunos[0]);
+//console.log(alunos[2]);
+
+//alunos[0] = "Eduardo";
+//alunos[3] = "Luiza";
+
+//alunos[alunos.length] = "Luiza";
+///alunos[alunos.length] = "Fábio";
+//alunos[alunos.length] = "Luana";
+
+//alunos.push("Luiza"); = Adiciona no fim
+
+//alunos.unshift("Fábio");
+//alunos.unshift("Luiza");
+
+//const removido = alunos.shift();
+//console.log(removido);
+//console.log(alunos);
+
+//delete alunos[1];
+//console.log(alunos);
+//console.log(alunos.slice(0, -2));
