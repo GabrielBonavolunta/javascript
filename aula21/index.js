f487e7f6cca7fe707d4a04e968150000a29db390
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-04-11 18:19:45 -0300

    Commits Aulas 21-22

---
+++ aula21/index.js
@@ -0,0 +1,17 @@
+/* 
+    Operadores de Comparação
+
+    > Maior que
+    >= Maior ou igual a
+    < Menor que
+    <= Menor ou igual a
+    == Igual (valor) **********************
+    === Igualdade Estrita (valor e tipo)
+    != Diferente (valor) ************************
+    !== Diferente Estrito (valor e tipo)
+*/
+
+const num1 = 10; // Number
+const num2 = '10'; // String
+const comp = num1 != num2;
+console.log(comp);
---
+++ aula22/index.js
@@ -0,0 +1,13 @@
+/* 
+    Operadores Lógicos
+
+    && AND -> E (Todas as expressões precisam ser verdadeiras para retornar true)
+    || OR -> OU
+    ! NOT -> NÃO
+
+*/
+
+const expressaoAnd = true && true && false && true;
+console.log(expressaoAnd);
+const expressaoOr = true || true || false || true;
+console.log(expressaoOr);
