3f0c5bb41f7fe892259d082ee95d365756006d45
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-05-12 16:47:42 -0300

    Commit Aula 27-28

---
+++ aula27/index.js
@@ -0,0 +1,17 @@
+// ? (Verdadeiro) : (Falso)
+
+const pontuacaoUsuario = 1000;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? "Usuário VIP!" : "Usuário normal";
+
+const corUsuario = null;
+const corPadrao = corUsuario || "Preta";
+
+
+console.log(nivelUsuario);
+
+//if (pontuacaoUsuario >= 1000){
+//console.log("Usuário VIP!");
+//} else {
+//   console.log("Usuário normal!");
+//}
